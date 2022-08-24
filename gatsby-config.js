ef516ecf11e43e72d53803c47429527d333923a0
amos-kibet <kibetamos97@gmail.com>
2022-08-24 15:45:52 +0300

    initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,53 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/gatsby-config/
+ */
+
+module.exports = {
+  siteMetadata: {
+    title: "Personal Portfolio",
+    description:
+      "This is a personal Portfolio Site Built with Gatsby and served by Strapi CMS API",
+    author: "@amos-kibet",
+    twitterUsername: "@amoskibet_",
+    image: "/twitter-img.png",
+    siteUrl: "https://fly.dev", // to update once deployed
+  },
+  plugins: [
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sitemap`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `assets`,
+        path: `${__dirname}/src/assets/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-strapi`,
+      options: {
+        apiURL: `http://localhost:1337`,
+        queryLimit: 1000, // Default to 100
+        contentTypes: [`jobs`, `projects`, `blogs`],
+        singleTypes: [`about`],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-webfonts`,
+      options: {
+        fonts: {
+          google: [
+            {
+              family: "Roboto",
+              variants: ["400", "700"],
+            },
+            { family: "Open Sans" },
+          ],
+        },
+      },
+    },
+  ],
+};
---
+++ src/constants/socialLinks.js
@@ -0,0 +1,33 @@
+import React from "react"
+import {
+  FaGitHub,
+  FaLinkedin,
+} from "react-icons/fa"
+
+const data = [
+  {
+    id: 1,
+    icon: <FaGitHub className="social-icon"></FaGitHub>,
+    url: "https://www.github.com/amos-kibet",
+  },
+  {
+    id: 2,
+    icon: <FaLinkedin className="social-icon"></FaLinkedin>,
+    url: "https://www.linkedin.com/in/amos-kibet",
+  }
+]
+const links = data.map(link => {
+  return (
+    <li key={link.id}>
+      <a href={link.url} className="social-link">
+        {link.icon}
+      </a>
+    </li>
+  )
+})
+
+export default ({ styleClass }) => {
+  return (
+    <ul className={`social-links ${styleClass ? styleClass : ""}`}>{links}</ul>
+  )
+}
